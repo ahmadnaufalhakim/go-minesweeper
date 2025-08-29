4dabe498d4cee40c2973d9aa562b2644769c2909
ahmadnaufalhakim <ahmadnaufalhakim@gmail.com>
2025-08-29 23:41:01 +0700

    add DrawCentered function in ui

--- ui.go
+++ ui.go
@@ -21,6 +21,12 @@ func DrawString(screen tcell.Screen, x, y int, style tcell.Style, s string) {
 	}
 }
 
+func DrawCentered(screen tcell.Screen, y int, style tcell.Style, str string) {
+	w, _ := screen.Size()
+	start := (w-len(str))/2 - len(str)%2
+	DrawString(screen, start, y, DefaultStyle, str)
+}
+
 func (s *Sprite) Draw(screen tcell.Screen, style tcell.Style) {
 	screen.SetContent(s.X, s.Y, s.Char, nil, style)
 }
