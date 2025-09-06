47b7b784b5ade68e58cf93be9be485c5c269c9d9
ahmadnaufalhakim <ahmadnaufalhakim@gmail.com>
2025-09-06 16:58:11 +0700

    feat: add function to draw background image

--- ui.go
+++ ui.go
@@ -1,6 +1,9 @@
 package main
 
-import "github.com/gdamore/tcell/v2"
+import (
+	"github.com/ahmadnaufalhakim/go-minesweeper/assets"
+	"github.com/gdamore/tcell/v2"
+)
 
 type Sprite struct {
 	Char rune
@@ -29,4 +32,33 @@ func DrawCentered(screen tcell.Screen, y int, style tcell.Style, str string) {
 
 func (s *Sprite) Draw(screen tcell.Screen, style tcell.Style) {
 	screen.SetContent(s.X, s.Y, s.Char, nil, style)
+}
+
+func DrawBackground(screen tcell.Screen, name string, negative bool) {
+	w, h := screen.Size()
+	bg, ok := assets.LoadBackground(name)
+	if !ok {
+		return
+	}
+	for y := range h {
+		for x := range w {
+			xRef := int(float64(x * bg.Bounds().Max.X / w))
+			yRef := int(float64(y * bg.Bounds().Max.Y / h))
+			rRef, gRef, bRef, _ := bg.At(xRef, yRef).RGBA()
+
+			r := int32(rRef >> 8)
+			g := int32(gRef >> 8)
+			b := int32(bRef >> 8)
+			if negative {
+				r = 255 - r
+				g = 255 - g
+				b = 255 - b
+			}
+
+			screen.SetContent(
+				x, y,
+				' ', nil,
+				DefaultStyle.Background(tcell.NewRGBColor(r, g, b)))
+		}
+	}
 }
